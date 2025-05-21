7e22e33801ea9a6aada5b2dd42500c668600cb07
Gabriel Kassarp <gkassarp@gmail.com>
2025-05-21 10:17:54 +0200

    more packages installed

--- gourmand/screens/Home.js
+++ gourmand/screens/Home.js
@@ -5,8 +5,10 @@ import { SafeAreaView } from 'react-native-web'
 
 export default function Home() {
   return (
-   <SafeAreaView>
+   <SafeAreaView style={{backgroundColor: "gray", flex: 1}}>
+    <View style={{ backgroundColor: "white" , padding: 15}}>
     <HeaderTabs/>
+    </View>
    </SafeAreaView>
   )
 }
