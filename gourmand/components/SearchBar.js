c1a482f0a5da779a47dbfe1308c689844c6fd8d9
Gabriel Kassarp <gkassarp@gmail.com>
2025-05-21 10:30:17 +0200

    searchbar UI (non functional rn)

--- gourmand/components/SearchBar.js
+++ gourmand/components/SearchBar.js
@@ -1,10 +1,54 @@
-import React from 'react'
-import { View } from 'react-native'
-
+import React from "react";
+import { View } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
 export default function Searchbar() {
   return (
-   <View>
-    
-   </View>
-  )
+    <View style={{ marginTop: 15, flexDirection: "row" }}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        styles={{
+          textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: 700,
+            marginTop: 8,
+          },
+          textInputContainer: {
+            backgroundColor: "#eee",
+            borderRadius: 50,
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
+          },
+        }}
+        renderLeftButton={() => (
+          <View style={{ marginLeft: 10 }}>
+            <Ionicons name="location-sharp" size={24} color="black" />
+          </View>
+        )}
+        renderRightButton={() => (
+          <View
+            style={{
+              marginRight: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              borderRadius: 30,
+              backgroundColor: "white",
+              padding: 9,
+            }}
+          >
+            <AntDesign
+              name="clockcircle"
+              size={16}
+              color="black"
+              marginRight={8}
+            />
+            <Text>Search</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
--- gourmand/screens/Home.js
+++ gourmand/screens/Home.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import { View } from 'react-native'
-import HeaderTabs from '../components/HeaderTabs'
-import { SafeAreaView } from 'react-native-web'
+
+import { View } from "react-native";
+import HeaderTabs from "../components/HeaderTabs";
+import { SafeAreaView } from "react-native-web";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
-   <SafeAreaView style={{backgroundColor: "gray", flex: 1}}>
-    <View style={{ backgroundColor: "white" , padding: 15}}>
-    <HeaderTabs/>
-    </View>
-   </SafeAreaView>
-  )
+    <SafeAreaView style={{ backgroundColor: "gray", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+    </SafeAreaView>
+  );
 }
