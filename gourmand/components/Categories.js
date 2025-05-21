e6bd66aa1f67736f8cde8a75a6551c3cfb4b6b1f
Gabriel Kassarp <gkassarp@gmail.com>
2025-05-21 11:33:25 +0200

    set up food/drink categories

--- gourmand/components/Categories.js
+++ gourmand/components/Categories.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { ScrollView, View } from "react-native-web";
 
+const items = [
+  {
+    image: require("../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    text: "Pick-Up",
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    text: "Bakery items",
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    text: "Fast food",
+  },
+
+  {
+    image: require("../assets/images/coffee.png"),
+    text: "Cafes",
+  },
+  {
+    image: require("../assets/images/desserts.png"),
+    text: "Desserts",
+  },
+];
 export default function Categories() {
   return (
-  <View>
-    
-  </View>
-  )
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={{
+        marginTop: 8,
+        backgroundColor: "white",
+        paddingVertical: 8,
+        paddingLeft: 20,
+      }}
+    >
+      {items.map((item, index) => (
+        <View
+          key={index}
+          style={{
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={item.image}
+            style={{ width: 50, height: 50, resizeMode: "contain" }}
+          />
+          <Text style={{ fontSize: 18, fontWeight: "900" }}>{item.text}</Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
 }
